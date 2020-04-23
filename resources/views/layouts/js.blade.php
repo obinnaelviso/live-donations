a59903448ee4cd49f4667727ac1c017b575c93cc
Obinna Elvis <dreamor47@gmail.com>
2020-04-23 16:21:42 +0100

    change live chat

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -14,5 +14,15 @@
 <script src="/js/mail-script.js"></script>
 <script src="/js/custom.js"></script>
 <script type="module" src="/vendors/countUp/countUp.min.js"></script>
-<script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '372498e8-a28f-46f8-805d-8aef280f68d4', f: true }); done = true; } }; })();</script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '53523ae3e060943d82a185a41d7ba73b17565a09';
+    window.smartsupp||(function(d) {
+      var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+      s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+      c.type='text/javascript';c.charset='utf-8';c.async=true;
+      c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
 @yield('moreJS')
